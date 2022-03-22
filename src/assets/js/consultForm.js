4a895a6c43dfda67ed3fb4f54a5b3f94ba21d1a1
Jorge Luiz <jorgeluizti@hotmail.com>
2022-03-21 21:14:35 -0300

    factoring code

--- src/assets/js/consultForm.js
+++ src/assets/js/consultForm.js
@@ -4,6 +4,8 @@
 const cpf = document.querySelector("#cpf") //variável de interface
 let search //variável de ambiente
 
+const inputBtn = document.querySelector('.input-btn')
+
 const showData = (result) => {
     for (const campo in result) {
         if (document.querySelector("#" + campo)) { // validação: verificar se meu input=ID tem os mesmos campos que vem do Fetch. E o campo que meu input=ID não tiver ( descarta )
@@ -14,7 +16,7 @@ const showData = (result) => {
     }
 }
 
-cpf.addEventListener("blur", async (event) => {  // evento "blur" captura o que foi digitado no campo
+inputBtn.addEventListener("click", async (event) => {  // evento "blur" captura o que foi digitado no campo
     // if (event.keyCode === 13)
     if (cpf.value.includes(".")) {
         search = cpf.value.replace(".", "")
